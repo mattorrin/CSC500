4e1d83a9eb8604cd0d8c678f5eff3792a81f08c0
mattheworrin <matthew.orrin@csuglobal.edu>
2025-10-24 10:43:43 -0600

    Update Mod7 - Courses.py

--- Mod7 - Courses.py
+++ Mod7 - Courses.py
@@ -1,17 +1,32 @@
-#
-room_num = {'CSC101': '3004', 'CSC102': '4501',
-            'CSC103': '6755', 'NET110': '1244',
-            'COM241': '1411'}
+#get_course_info takes input and searches for related info for that course
+def get_course_info():
 
-instructors = {'CSC101': 'Haynes', 'CSC102': 'Alvarado',
-            'CSC103': 'Rich', 'NET110': 'Burke',
-            'COM241': 'Lee'}
+    course_nums = {'CSC101': {"room": '3004', "Instructor": "Haynes",
+                          "meet time": "8:00 a.m."},
+               'CSC102': {"room": '4501', "Instructor": "Alvarado",
+                          "meet time": '9:00 a.m.'},
+               'CSC103': {"room": '6755', "Instructor": "Rich",
+                          "meet time": '10:00 a.m.'},
+               'NET110': {"room": '1244', "Instructor": "Burke",
+                          "meet time": '11:00 a.m.'},
+               'COM241': {"room": '1411', "Instructor": "Lee",
+                          "meet time": '1:00 p.m.'}}
 
-meet_times = {'CSC101': '8:00 a.m.', 'CSC102': '9:00 a.m.',
-            'CSC103': '10:00 a.m.', 'NET110': '11:00 a.m.',
-            'COM241': '1:00 p.m.'}
+#Determines if input is valid
+    valid_course = False
+    while valid_course == False:               
+        course_number = input('State course you are interested in: ').upper()   #handles and case by making it uppercase
+        
+#If entry is in dictionary, then it will output each related value then change valid_course to True to end the loop
+        if course_number in course_nums:
+            info = course_nums[course_number]
+            print('\n{} is in room {}, with Instructor {}, at {}'.format(course_number, info["room"],
+                                                                 info["Instructor"],
+                                                                 info["meet time"]))
+            valid_course = True
+        
+        else:
+            print('Not a valid entry, please try again!\n')
 
-course_nums = {'CSC101': {
-    "Room Number": '3004', "Instructor": "Haynes",
-    "Meet Time": "8:00 a.m."}
-               'CSC102'
+#Function call
+get_course_info()
